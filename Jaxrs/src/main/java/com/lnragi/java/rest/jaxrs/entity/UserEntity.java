c431f97ce2cd81db98d3e7fd0cb31d7cc35f9856
laxminarsaiah <rln.reach@gmail.com>
2018-01-11 21:51:45 +0530

    properties added

--- Jaxrs/src/main/java/com/lnragi/java/rest/jaxrs/entity/UserEntity.java
+++ Jaxrs/src/main/java/com/lnragi/java/rest/jaxrs/entity/UserEntity.java
@@ -1,8 +1,72 @@
 package com.lnragi.java.rest.jaxrs.entity;
+
 /**
  * @author Laxminarsaiah Ragi
  *
  */
 public class UserEntity {
+
+	private Integer id;
+	private String firstName;
+	private String lastName;
+	private Integer phone;
+	private String email;
+
+	
+	
+	public UserEntity(Integer id, String firstName, String lastName, Integer phone, String email) {
+		super();
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.phone = phone;
+		this.email = email;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public Integer getPhone() {
+		return phone;
+	}
+
+	public void setPhone(Integer phone) {
+		this.phone = phone;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	@Override
+	public String toString() {
+		return String.format("UserEntity [id=%s, firstName=%s, lastName=%s, phone=%s, email=%s]", id, firstName,
+				lastName, phone, email);
+	}
 
 }
