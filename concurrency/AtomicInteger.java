427833e066fd2888daf8f0967571d758f2e5dde7
Laxminarsaiah Ragi <rln.reach@gmail.com>
2018-01-12 14:20:03 +0530

    atomicIntegerUpdateAndGetMethodTest() added

--- concurrency/AtomicInteger.java
+++ concurrency/AtomicInteger.java
@@ -43,7 +43,22 @@ public class AtomicDatatypes {
 		System.out.println("Incremented Value : " + ai.get());
 
 	}
-	
+		public static void atomicIntegerUpdateAndGetMethodTest() {
+		ai.set(0);
+		ExecutorService executorService = Executors.newFixedThreadPool(10);
+		IntStream.range(0, NO_OF_INC).forEach(i -> {
+			Runnable task = () -> ai.updateAndGet(n -> n + 2);
+			executorService.submit(task);
+		});
+		executorService.shutdown();
+		try {
+			executorService.awaitTermination(120, TimeUnit.SECONDS);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		System.out.println("Updated value : " + ai.get());
+
+	}
 	
 	public static void atomicIntegerDecrementAndGetMethodTest() {
 		ai.set(0);
@@ -60,9 +75,10 @@ public class AtomicDatatypes {
 	}
 
 	public static void main(String[] args) {
-		//atomicIntegerAccumulateMethodTest();
-		//atomicIntegerIncrementAndGetMethodTest();
+		atomicIntegerAccumulateMethodTest();
+		atomicIntegerIncrementAndGetMethodTest();
 		atomicIntegerDecrementAndGetMethodTest();
+		atomicIntegerUpdateAndGetMethodTest();
 	}
 
 }
