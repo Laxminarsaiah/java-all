7413196ccff05e7e71632501faafb7f9f455cb3d
Laxminarsaiah Ragi <rln.reach@gmail.com>
2018-01-12 12:43:54 +0530

    Update and rename AtomicInteger to AtomicInteger.java

---
+++ AtomicInteger.java
@@ -0,0 +1,68 @@
+package com.lnragi.java.concurrency;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.IntStream;
+
+public class AtomicDatatypes {
+
+	private final static Integer NO_OF_INC = 5;
+	private final static AtomicInteger ai = new AtomicInteger();
+
+	public static void atomicIntegerAccumulateMethodTest() {
+		ai.set(0);
+		ExecutorService executorService = Executors.newFixedThreadPool(10);
+		IntStream.range(0, NO_OF_INC).forEach(i -> {
+			Runnable mytask = () -> ai.accumulateAndGet(i, (a, b) -> a + b);
+			executorService.submit(mytask);
+
+		});
+
+		executorService.shutdown();
+		try {
+			executorService.awaitTermination(120, TimeUnit.SECONDS);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		System.out.println("Accumulated Value  : " + ai.get());
+
+	}
+
+	public static void atomicIntegerIncrementAndGetMethodTest() {
+		ai.set(0);
+		ExecutorService executorService = Executors.newFixedThreadPool(10);
+		IntStream.range(0, NO_OF_INC).forEach( i -> executorService.submit(ai::incrementAndGet));
+		executorService.shutdown();
+		try {
+			executorService.awaitTermination(120, TimeUnit.SECONDS);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		System.out.println("Incremented Value : " + ai.get());
+
+	}
+	
+	
+	public static void atomicIntegerDecrementAndGetMethodTest() {
+		ai.set(0);
+		ExecutorService executorService = Executors.newFixedThreadPool(10);
+		IntStream.range(0, NO_OF_INC).forEach( i -> executorService.submit(ai::decrementAndGet));
+		executorService.shutdown();
+		try {
+			executorService.awaitTermination(120, TimeUnit.SECONDS);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		System.out.println("Decremented value : " + ai.get());
+
+	}
+
+	public static void main(String[] args) {
+		//atomicIntegerAccumulateMethodTest();
+		//atomicIntegerIncrementAndGetMethodTest();
+		atomicIntegerDecrementAndGetMethodTest();
+	}
+
+}
