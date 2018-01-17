878b94ca3fc60049aecd59dc2ecd46593f9b699d
Laxminarsaiah Ragi <rln.reach@gmail.com>
2018-01-17 16:07:46 +0530

    Update UserServiceImpl.java

--- Jaxrs/src/main/java/com/lnragi/java/rest/jaxrs/service/UserServiceImpl.java
+++ Jaxrs/src/main/java/com/lnragi/java/rest/jaxrs/service/UserServiceImpl.java
@@ -19,33 +19,28 @@ public class UserServiceImpl implements UserService {
 
 	@Override
 	public UserEntity addUser(UserEntity user) {
-		UserDao userDao = new UserDAOImpl();
-		return userDao.addUser(user);
+		return new UserDAOImpl().addUser(user);
 	}
 
 	@Override
 	public List<UserEntity> findAll() {
-		UserDao userDao = new UserDAOImpl();
-		return userDao.findAll();
+		return new UserDAOImpl().findAll();
 	}
 
 	@Override
 	public UserEntity findById(Integer id) {
-		UserDao userDao = new UserDAOImpl();
-		return Optional.ofNullable(userDao.findById(id)).orElseThrow(
+		return Optional.ofNullable(new UserDAOImpl().findById(id)).orElseThrow(
 				() -> new UserNotFoundException(id));
 	}
 
 	@Override
 	public UserEntity updateUser(UserEntity usr) {
-		UserDao userDao = new UserDAOImpl();
-		return userDao.updateUser(usr);
+		return new UserDAOImpl().updateUser(usr);
 	}
 
 	@Override
 	public void deleteUser(Integer id) {
-		UserDao userDao = new UserDAOImpl();
-		userDao.deleteUser(id);
+		new UserDAOImpl().deleteUser(id);
 	}
 
 }
