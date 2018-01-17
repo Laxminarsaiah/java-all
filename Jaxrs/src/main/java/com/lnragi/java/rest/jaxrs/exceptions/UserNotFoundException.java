061e734ab5c46217ada9fe2cd9b74f19d3d4f29d
Laxminarsaiah Ragi <rln.reach@gmail.com>
2018-01-17 16:24:38 +0530

    Update UserNotFoundException.java

--- Jaxrs/src/main/java/com/lnragi/java/rest/jaxrs/exceptions/UserNotFoundException.java
+++ Jaxrs/src/main/java/com/lnragi/java/rest/jaxrs/exceptions/UserNotFoundException.java
@@ -1,25 +1,52 @@
 package com.lnragi.java.rest.jaxrs.exceptions;
 
 public class UserNotFoundException extends RuntimeException {
-
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
 
-	private Integer userId;
+	private Integer id;
+	private String firstName;
+	private String lastName;
+	private Integer phone;
+	private String email;
 
-	public UserNotFoundException() {
+	public UserNotFoundException(Integer id) {
+		super("User with '#" + id + "' was not found");
+		this.id = id;
 	}
 
-	public UserNotFoundException(Integer uid) {
-		super("User #" + uid + " was not found");
-		this.userId = uid;
-	}
-
-
-	public Integer getUserId() {
-		return userId;
+	
+	
+	
+	public UserNotFoundException(String firstName, String lastName) {
+		super("User with '" + firstName.concat(" ").concat(lastName)
+				+ "'  was not found");
+		this.firstName = firstName;
+		this.lastName = lastName;
 	}
 	
+	
+	
+	public Integer getId() {
+		return id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public Integer getPhone() {
+		return phone;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
 }
